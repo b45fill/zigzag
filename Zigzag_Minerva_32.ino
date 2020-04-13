4f0392544d9efc8b847b25b7f8049ecb9396bdcf
b45fill <50545789+b45fill@users.noreply.github.com>
2020-04-13 08:46:34 +0300

    Update Zigzag_Minerva_32.ino

--- Zigzag_Minerva_32.ino
+++ Zigzag_Minerva_32.ino
@@ -1,3 +1,4 @@
+
 #include <LiquidCrystal.h>
 #include <AccelStepper.h>
 
@@ -31,167 +32,189 @@ int getPressedButton() {
   }
   return BUTTON_NONE;
 }
-AccelStepper Stepper1(1,6,5); //использует пин 12 и 13 для dir и step, 1 - режим "external driver" (A4988)
+AccelStepper Stepper1(1,22,23); //использует пин 12 и 13 для dir и step, 1 - режим "external driver" (A4988)
 
-const int PIN_BUTTON = 7; //сигнал с оптопары положения вала
+const int PIN_BUTTON = 21; //сигнал с оптопары положения вала
 int dir = 1; //используется для смены направления
+int dir1 = 1; //используется для выхода в ноль при старте программы 
+int place = 1; //используется для фиксайии серидины зигзага
+int place1 = 1; //используется для фиксайии нуля прямой строчки
 int zigzag_mode = 1; //используется для смены вида зигзага
-int steps1 = 40; //используется для ширины 2x
-int steps2 = 15; //используется для ширины 4x
-int num = 1; //количество шагов в 4*укольном зигзаге
+int steps1 = 40; //используется для шага меду уколами 2x
+int steps2 = 15; //используется для шага меду уколами 4x
+int num = 1; //номер шага в 4*укольном зигзаге
 int Sign = 2; //срабатывание позиционера
 
 void setup() {
- 
 
 
-Stepper1.setSpeed(10000); //устанавливаем максимальную скорость вращения ротора двигателя (шагов/секунду)
-Stepper1.setAcceleration(50000); //устанавливаем ускорение (шагов/секунду^2) 
 
-    lcd.begin(16, 2);
-  lcd.print("Zigzag ");
+  Stepper1.setSpeed(100000); //устанавливаем максимальную скорость вращения ротора двигателя (шагов/секунду)
+  Stepper1.setAcceleration(500000); //устанавливаем ускорение (шагов/секунду^2)
+
+  dir1=100;
+  Stepper1.move(dir1);  //двигаемя вправо до упора
+  Stepper1.run();
+  delay(200); //задержка
+  dir1=-50;
+  Stepper1.move(dir1);//двигаемя обратно до середины 
+  Stepper1.run();
+  delay(200); //задержка
+  place = Stepper1.currentPosition();  //запоминаем положение середины
+  place1 = Stepper1.currentPosition(); //запоминаем положение середины
+
+
+  lcd.begin(16, 2);            //начальная загрузка экрана
+  lcd.print("Zigzag Two");
   lcd.setCursor(0, 1);
-      lcd.print("Width");
-      lcd.setCursor(6, 1);
-        lcd.print(steps1);}
- 
+  lcd.print("Width 40");
 
+}
 
 
 void loop(){
-  
- 
-  
- button = getPressedButton();
+
+
+
+  button = getPressedButton(); //цикл меню
   switch (button) {
-    case BUTTON_RIGHT: // при 
-      
-     Stepper1.move(-1);
-       Stepper1.run();
-      delay(200); //задержка
-      break;
-      
-    case BUTTON_LEFT:
-     
-       Stepper1.move(-1);
-       Stepper1.run();
-       delay(200); //задержка 
-      break;
-    case BUTTON_UP:
-       if (zigzag_mode == 1){
-        steps1=steps1+1;
-        lcd.setCursor(6, 1);
-         lcd.print("      ");
-        lcd.setCursor(6, 1);
-        lcd.print(steps1);}
-        else 
-        { steps2=steps2+1;
-        lcd.setCursor(6, 1);
-         lcd.print("      ");
-        lcd.setCursor(6, 1);
-        lcd.print(steps2);}
-        
-      delay(200); //задержка 
-      break;
-    case BUTTON_DOWN:
+  case BUTTON_RIGHT: // при 
+    dir1=-1;
+    Stepper1.move(dir1);
+    Stepper1.run();
+    delay(200); //задержка
+    if (zigzag_mode == 0) place1 = Stepper1.currentPosition();
+    break;
+
+  case BUTTON_LEFT:
+    dir1=1;
+    Stepper1.move(dir1);
+    Stepper1.run();
+    delay(200); //задержка
+    if (zigzag_mode == 0) place1 = Stepper1.currentPosition(); 
+    break;
+  case BUTTON_UP:
+    if (zigzag_mode == 1){
+      steps1=steps1+1;
       lcd.setCursor(6, 1);
-         lcd.print("      ");
-      if (zigzag_mode == 1){
-        steps1=steps1-1;
-        lcd.setCursor(6, 1);
-        lcd.print(steps1);}
-        else 
-        { steps2=steps2-1;
-        lcd.setCursor(6, 1);
-        lcd.print(steps2);}
-       delay(200);
-      break;
-    case BUTTON_SELECT:
-    
-     if (zigzag_mode == 2){
-                lcd.setCursor(0, 0);
-                lcd.print("                 ");
-                lcd.setCursor(0, 0);
-                 lcd.print("Line Stitch");
-                 lcd.setCursor(6, 1);
-                  lcd.print("      ");
-                  delay(100);
-              zigzag_mode = 0 ;} 
-             else
-             if (zigzag_mode == 0){
-                lcd.setCursor(0, 0);
-                lcd.print("                 ");
-                lcd.setCursor(0, 0);
-                 lcd.print("Zigzag  ");
-                 zigzag_mode = zigzag_mode + 1;
-                  lcd.setCursor(6, 1);
-                  lcd.print("      ");
-                   lcd.setCursor(6, 1);
-                   lcd.print(steps1);
-                delay(100); }
-                 else           {
-                lcd.setCursor(0, 0);
-                lcd.print("                 ");
-                lcd.setCursor(0, 0);
-                 lcd.print("Zigzag 4");
-                 
-                lcd.setCursor(6, 1);
-                lcd.print("      ");
-                lcd.setCursor(6, 1);
-                lcd.print(steps2);
-                  delay(100);
-              zigzag_mode = zigzag_mode + 1; } 
-    
-           
-       
-     //  lcd.setCursor(12, 1);
-      //          lcd.print(zigzag_mode);       
-      
-       delay(200);
-       
- 
-      break;
-  }
- 
- 
- 
- if(Stepper1.distanceToGo()==0){ //проверка, отработал ли двигатель предыдущее движение
-  int buttonState = digitalRead(PIN_BUTTON); 
-      if (buttonState == 0){
- 
-                      if (Sign == 1){
-                                    
-                                    
-                                    if (zigzag_mode  == 1)   {                 
-                                          Stepper1.move(steps1*dir); //устанавливает  перемещение
-                                          dir = dir*(-1); //отрицательное значение dir, благодаря чему реализуется вращение в противоположном направлении 
-                                         
-                                          delay(1); //задержка 
-                                          Sign = 2;
-                                                  }
-              
-                                   
-                                    else  // зигзаг мода равен 2
-                                          if (num < 4) {
-                                              num = (num+1);
-                          
-                                               Stepper1.move(steps2*dir); //устанавливает  перемещение  
-                                               delay(5); //задержка
-                                               
-                                               Sign=2;   }
-                                               
-                                          else {
-                                             dir = dir*(-1); //отрицательное значение dir, благодаря чему реализуется вращение в противоположном направлении
-                                             num = 1; }
-                        }  
-                        }
-       else      Sign = 1;
-        
-      
-                        }   
-                         
-    Stepper1.run(); //запуск шагового двигателя.     
- 
-     
-                 
+      lcd.print("      ");
+      lcd.setCursor(6, 1);
+      lcd.print(steps1);
+    }
+    else 
+    { 
+      steps2=steps2+1;
+      lcd.setCursor(6, 1);
+      lcd.print("      ");
+      lcd.setCursor(6, 1);
+      lcd.print(steps2);
+    }
+
+    delay(200); //задержка 
+    break;
+  case BUTTON_DOWN:
+    lcd.setCursor(6, 1);
+    lcd.print("      ");
+    if (zigzag_mode == 1){
+      steps1=steps1-1;
+      lcd.setCursor(6, 1);
+      lcd.print(steps1);
+    }
+    else 
+    { 
+      steps2=steps2-1;
+      lcd.setCursor(6, 1);
+      lcd.print(steps2);
+      delay(200);
+    } 
+    break;
+
+
+  case BUTTON_SELECT:
+    if (zigzag_mode == 2){ //прямая строчка
+      lcd.setCursor(0, 0);
+      lcd.print("                 ");
+      lcd.setCursor(0, 0);
+      lcd.print("Straight Stitch");
+
+      zigzag_mode = 0 ; 
+      Stepper1.moveTo(place1);
+      Stepper1.run();
+      delay(200);
+    }
+    else {
+      if (zigzag_mode == 0) //обычный зигзаг
+      {
+        lcd.setCursor(0, 0);
+        lcd.print("                 ");
+        lcd.setCursor(0, 0);
+        lcd.print("Zigzag Two ");
+        Stepper1.moveTo( place + 0.5 * steps1);
+        Stepper1.run();
+        delay(200); 
+      }
+      if (zigzag_mode == 1)  //4х укольный зигзаг 
+      {
+        lcd.setCursor(0, 0);
+        lcd.print("                 ");
+        lcd.setCursor(0, 0);
+        lcd.print("Zigzag Four");
+        Stepper1.moveTo( place + 0.75 * steps2);
+        Stepper1.run();
+        delay(200);
+        num = 1; 
+      } 
+      zigzag_mode = zigzag_mode + 1;      
+    }  
+    break;
+
+
+  }  //конец меню
+
+
+    //функция перемещения игловода
+  if(Stepper1.distanceToGo()==0){ //проверка, отработал ли двигатель предыдущее движение
+    int buttonState = digitalRead(PIN_BUTTON); 
+    if (buttonState == 0){
+
+      if (Sign == 1){
+
+
+        if (zigzag_mode  == 1)   {                 
+          Stepper1.move(steps1*dir); //устанавливает  перемещение
+          dir = dir*(-1); //отрицательное значение dir, благодаря чему реализуется вращение в противоположном направлении 
+
+          delay(3); //задержка на 1 секунду
+          Sign = 2;
+        }
+
+
+        else  
+          if (zigzag_mode  == 2) { // зигзаг мода равен 2
+          if (num < 4) {
+            num = (num+1);
+
+            Stepper1.move(steps2*dir); //устанавливает следующее перемещение  > противоположное направление)
+            delay(3); //задержка
+
+            Sign=2;   
+          }
+
+          else {
+            dir = dir*(-1); //отрицательное значение dir, благодаря чему реализуется вращение в противоположном направлении
+            num = 1; 
+          }
+        }
+      } 
+    }
+    else      Sign = 1;
+
+
+  }   
+
+  Stepper1.run(); //запуск шагового двигателя.     
+
+
+
 } 
+
